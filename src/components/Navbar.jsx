66d86f1b9465a651bfc8067a9405f874e5a54400
Felipe <felipenn27@gmail.com>
2026-02-03 19:16:14 -0300

    feat(navbar): create fixed desktop navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import { Link } from 'react-router' // imports Link to don't reload the page when changing the path on the URL
+import '../style/Navbar.module.css'
 
 function Navbar() {
   return (
-    <div className='bg-white p-10'>
-      <h1 className="text-white" >
-        TEXT-HERE
-      </h1>
+    <div className='fixed flex w-screen justify-center'>
+      <nav className='rounded-b-3xl de p-7 bg-neutral-500 px-40 text-3xl'>
+        <ul className='flex justify-between gap-40'>
+          <li>
+            <Link className='' to='/'> about-me</Link>
+          </li>
+          <li>
+            <Link className='' to='/'> projects </Link>
+          </li>
+          <li>
+            <Link className='' to='/'> skills </Link>
+          </li>
+          <li>
+            <Link className='' to='/'> socials </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
