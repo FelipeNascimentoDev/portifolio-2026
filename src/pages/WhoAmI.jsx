054ab31af46d7962702b9eaef13871af23d36729
Felipe <felipenn27@gmail.com>
2026-02-23 15:51:15 -0300

    feat(page): creates a new page for the who am I section

---
+++ src/pages/WhoAmI.jsx
@@ -0,0 +1,17 @@
+
+function WhoAmI() {
+  return (
+    <div>
+      <div className="bg-[url('./assets/Section_2.jpg')]
+        bg-cover bg-center bg-no-repeat 
+        h-full w-full"
+      >
+        <div className="flex justify-center">
+          <h1 className="py-96 text-4xl">- in development -</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WhoAmI
