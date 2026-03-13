96db7bd4faa974b160cd98aa33ff1908e1d32b8f
Felipe <felipenn27@gmail.com>
2026-03-13 11:48:34 -0300

    feat(data): rename some of the titles

--- src/data/carouselContent.js
+++ src/data/carouselContent.js
@@ -1,7 +1,7 @@
 
 export const carouselData = [
-  { id: 1, title: "Meal4You Recommendation System",
-    role: "Java Backend Developer",
+  { id: 1, title: "Full Backend from zero",
+    role: "Backend Developer : APIs & ML",
     bulletPoints: [
       "• Decreased login and register time by more then 50% after implementing Google Social with OAuth 2.0",
       "• Improved database query performance by implementing server-side pagination (10 items per request), ensuring that only the necessary data is retrieved for each page load and reducing unnecessary data transfer",
@@ -14,7 +14,7 @@ export const carouselData = [
     certificateLink: ""
   },
 
-  { id: 2, title: "International Database Design & Development",
+  { id: 2, title: "Cross-country Database",
     role: "International Mediator and DB Designer",
     bulletPoints: [
       "• I served as the leader of the Brazilian group, mediating and translating conversations between Brazilian colleagues and students from Indiana (USA)",
@@ -25,7 +25,7 @@ export const carouselData = [
     certificateLink: "https://siga.cps.sp.gov.br/cartorio/autenticador.aspx?ce95e85e-67f8-4245-bf3b-febe1ea74238"
   },
 
-  { id: 3, title: "Microsservices College Project",
+  { id: 3, title: "Node.js Microsservices",
     role: "Microsservices Developer",
     bulletPoints: [
       "• Integrated AI-powered classification into an event-driven microservices architecture by connecting the system to the Gemini API, enabling automatic categorization of reminders based on their content",
@@ -36,14 +36,14 @@ export const carouselData = [
     certificateLink: ""
   },
 
-  { id: 4, title: "2026 Portfolio - This website",
+  { id: 4, title: "Web Frontend",
     role: "React.js Developer",
     bulletPoints: [
       "• Applied the DRY (Don\u2019t Repeat Yourself) Principle by leveraging the JavaScript .map() method to eliminate duplicated code, improving code maintainability, readability, and scalability",
       "• Implemented React\u2019s useState Hook to manage component state and dynamically render content based on user interactions and application state changes",
       "• Integrated external APIs to extend system functionality, retrieving and displaying real-time data dynamically within the application interface",
       "• Deployed the frontend application on Vercel, leveraging the platform\u2019s infrastructure for fast and reliable hosting",
-      "• Utilized serverless functions to securely retrieve data from the API and configured environment variables to protect sensitive information"
+      "• Utilized serverless functions to securely retrieve data from APIs and configured environment variables to protect sensitive information"
     ],
     gitHubProjectLink: "https://github.com/FelipeNascimentoDev/portifolio-2026",
     certificateLink: ""
