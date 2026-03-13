f437c5f8e4bbcd086a84eb628b350dc15a6354ea
Felipe <felipenn27@gmail.com>
2026-03-13 11:51:46 -0300

    feat(page): create a new order for the elements in the page and implement images to the carousel

--- src/pages/MyProjects.jsx
+++ src/pages/MyProjects.jsx
@@ -28,35 +28,34 @@ function MyProjects() {
   }
 
 
-
   return (
-    <div className="bg-black h-full w-full
-    text-center -mt-1 pt-16">
+    <div className="bg-neutral-800 h-full w-full
+    text-center -mt-1">
 
-      <h1 className="xl:text-8xl lg:text-7xl text-6xl font-bold my-20 mx-10">
+      <h1 className="xl:text-8xl lg:text-7xl text-5xl font-bold sm:py-20 pt-24 pb-14 mx-10">
         Projects I've worked on...
       </h1>
 
-      <div className='flex flex-col w-full justify-center py-10'>
+      <div className='flex flex-col w-full justify-center pt-4 pb-10'>
 
-        <div className='flex border-white border-2 rounded-[45px] place-items-center gap-5 px-4 py-1 mx-5
-        self-center xl:w-[1100px] md:w-[700px] justify-between'>
-          
-          <button className='font-semibold text-6xl pb-3 pr-3 pl-2 rounded-full
+
+        <div className='flex  place-items-center sm:gap-5 sm:px-4 py-1
+        self-center xl:w-[800px] md:w-[700px] sm:w-[600px] w-full justify-between'>
+          <button className='sm:font-semibold text-6xl pb-3 pr-3 pl-2 rounded-full
           hover:drop-shadow-[0_0_4px_rgba(255,255,255)]' onClick={previous}>
             {"<"}
           </button>
-          <h2 className='text-[darkgoldenrod] font-bold lg:text-4xl sm:text-3xl text-xl px-2'>
+          <h2 className='font-bold lg:text-4xl sm:text-3xl text-2xl px-2'>
             {carouselData[carouselIndex - 1].title}
           </h2>
-          <button className='font-semibold text-6xl pb-3 pr-2 pl-3 rounded-full
+          <button className='sm:font-semibold text-6xl pb-3 pr-2 pl-3 rounded-full
           hover:drop-shadow-[0_0_4px_rgba(255,255,255)]' onClick={next}>
             {">"}
           </button>
-          
         </div>
 
-        <div className='flex gap-8 mt-10 mb-5 self-center'>
+
+        <div className='flex gap-8 mt-10 mb-7 self-center'>
           {
           carouselData.map((element, elementIndex) =>{
             return(
@@ -70,33 +69,39 @@ function MyProjects() {
         </div>
 
 
-        <div className='text-4xl my-5 justify-items-center'>
+        <div className='self-center my-3 mx-5 w-fit'>
+          <img src={`/carousel-img/carousel-image-id-${carouselIndex}.jpg`}
+          className='lg:w-[800px] lg:h-[800px] w-full h-full rounded-3xl'/>
+        </div>
 
-          <div className='flex justify-center place-items-center min-[500px]:gap-x-10 gap-5'>
+
+        <h3 className='place-self-center text-center md:text-4xl text-3xl my-4 mx-10'>
+          {carouselData[carouselIndex - 1].role}
+        </h3>
+
+
+        <div className='flex place-content-center items-center min-[500px]:gap-x-10 gap-5 py-4'>
           {/* Certificate Image + Link */}
-            {
-              carouselData[carouselIndex - 1].certificateLink != "" ?
-              <a href={carouselData[carouselIndex - 1].certificateLink} target="_blank">
-                <img src={cetificateImgLink} className='hover:-rotate-6 transition-transform duration-300
-                sm:w-[70px] sm:h-[70px] w-16 h-16 cursor-pointer'/>
-              </a> : ""
-            }
-          
+          {
+            carouselData[carouselIndex - 1].certificateLink != "" ?
+            <a href={carouselData[carouselIndex - 1].certificateLink} target="_blank">
+              <img src={cetificateImgLink} className='hover:-rotate-6 transition-transform duration-300
+              md:w-[70px] md:h-[70px] w-[55px] h-[55px] cursor-pointer'/>
+            </a> : ""
+          }
           {/* GitHub Image + Link */}
-            {
-              carouselData[carouselIndex - 1].gitHubProjectLink != "" ?
-              <a href={carouselData[carouselIndex - 1].gitHubProjectLink} target="_blank">
-                <img src={gitHubLogoLink} className='hover:-rotate-6 transition-transform duration-300
-                sm:w-[70px] sm:h-[70px] w-16 h-16 cursor-pointer'/>
-              </a> : ""
-            }
-          </div>
+          {
+            carouselData[carouselIndex - 1].gitHubProjectLink != "" ?
+            <a href={carouselData[carouselIndex - 1].gitHubProjectLink} target="_blank">
+              <img src={gitHubLogoLink} className='hover:-rotate-6 transition-transform duration-300
+              md:w-[70px] md:h-[70px] w-[55px] h-[55px] cursor-pointer'/>
+            </a> : ""
+          }
+        </div>
 
-          <p className='mt-12 grid grid-cols-1 gap-10 text-left'>
-            
-            <span className='place-self-center text-center mb-4 mx-3'>
-              Role: <u>{carouselData[carouselIndex - 1].role}</u>
-            </span>
+
+        <div className='text-4xl my-5 justify-items-center'>
+          <p className='mt-1 grid grid-cols-1 gap-10 text-left'>
 
             {carouselData[carouselIndex - 1].bulletPoints.map((item,itemIndex) => {
               return(
@@ -107,9 +112,9 @@ function MyProjects() {
             })}
 
           </p>
-
         </div>
         
+
       </div>
     </div>
   )
