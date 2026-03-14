e51a047eda39a573d35144f517210c8fd6bc6a27
Felipe <felipenn27@gmail.com>
2026-03-13 21:38:22 -0300

    fix(links): fix socials page image links

--- src/pages/Socials.jsx
+++ src/pages/Socials.jsx
@@ -7,13 +7,17 @@ function Socials() {
       <h1 className="text-black text-5xl font-semibold py-10">SOCIALS</h1>
 
       <div className="justify-center flex gap-10 py-16">
-        <img
-          src="https://img.icons8.com/color/96/linkedin.png"
-        />
-        <img 
-          src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-github-with-cat-logo-an-online-community-for-software-development-logo-color-tal-revivo.png"
-        />
-        <img src="https://img.icons8.com/color/96/credly.png"/>
+        <a target="_blank" href="https://LinkedIn.com/in/felipe-navas-nascimento">
+          <img src="https://img.icons8.com/color/96/linkedin.png"/>
+        </a>
+
+        <a target="_blank" href="https://github.com/FelipeNascimentoDev">
+          <img src="https://img.icons8.com/external-tal-revivo-color-tal-revivo/96/external-github-with-cat-logo-an-online-community-for-software-development-logo-color-tal-revivo.png"/>
+        </a>
+
+        <a target="_blank" href="https://credly.com/users/felipe-navas-nascimento/badges">
+          <img src="https://img.icons8.com/color/96/credly.png"/>
+        </a>
       </div>
 
     </div>
